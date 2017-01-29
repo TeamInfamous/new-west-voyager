ce1f5309222222ee4efae7b87beccf3657d1aab4
UnderratedDev <rajyudhvir29@gmail.com>
2017-01-28 22:32:05 -0800

    Completed the Randomise Username. First time you randomise it is laggy, and take about 3 seconds. // Used to lag every time. It works really quickly every other time. :)

--- app/src/main/java/a00961774/comp3717/bcit/ca/newwestvoyager/StartMenu.java
+++ app/src/main/java/a00961774/comp3717/bcit/ca/newwestvoyager/StartMenu.java
@@ -5,8 +5,15 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
+import android.content.res.AssetManager;
+import android.util.Log;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.Random;
 
 public class StartMenu extends AppCompatActivity {
+
+    ArrayList <String> file = new ArrayList <String> ();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,7 +22,35 @@ public class StartMenu extends AppCompatActivity {
         setContentView(R.layout.activity_start_menu);
     }
 
-    public void onClick_startMenuToHowToPlay(View view) {
+    private void dictionary () {
+        try {
+            AssetManager am = getAssets();
+            Scanner scan = new Scanner(am.open(String.format("randomWords.txt")));
+            while (scan.hasNextLine())
+                file.add(scan.nextLine());
+        } catch (Exception e) {
+            Log.d ("Broken :p", "Send Help. :p");
+        }
+    }
+
+    public void randomiseUsername (final View view) {
+        Random random   = new Random ();
+        if (file.size () == 0)
+            dictionary ();
+
+        String   rUsername = "";
+        int max = random.nextInt (3);
+
+        String[] arr = file.toArray (new String [0]);
+
+        for (int i = 0; i <= max; i++)
+            rUsername += arr [random.nextInt (arr.length)];
+
+        ((EditText)findViewById (R.id.usernameTextField)).setText (rUsername);
+
+    }
+
+    public void onClick_startMenuToHowToPlay (View view) {
         final Intent intent;
         EditText usernameEntry;
         String welcomeUsernameToastMsg;
